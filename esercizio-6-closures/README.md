1d3f4b3d84152eac60ccd78674116605143271e0
zeno9797 <126583332+zeno9797@users.noreply.github.com>
2023-04-05 22:45:35 +0200

    Add files via upload

---
+++ esercizio-6-closures/README.md
@@ -0,0 +1,8 @@
+# Exercise Closures
+
+Try to write your first `closure`. Write a function called `printName` and declare inside it a variable called `helloName`, the value of this variable will be: `"Hello John"`.
+Also declare a function named `inner` and return the `helloName` variable.
+The purpose is printing `"Hello John"` in the console.
+
+Tips:
+- You can learn more about this topic checking the following article: https://www.freecodecamp.org/news/closures-in-javascript/
---
+++ esercizio-6-closures/exercise.js
@@ -0,0 +1,22 @@
+// # Exercise Closures
+
+// Try to write your first `closure`. Write a function called `printName` and declare inside it a variable called `helloName`, the value of this variable will be: `"Hello John"`.
+// Also declare a function named `inner` and return the `helloName` variable.
+// The purpose is printing `"Hello John"` in the console.
+
+// Tips:
+// - You can learn more about this topic checking the following article: https://www.freecodecamp.org/news/closures-in-javascript/
+
+
+let helloName = "hello giu"
+
+function printName() {
+    let helloName = "Hello John"
+
+    function inner() {
+        return helloName
+    }
+    return inner
+}
+
+console.log(printName()())
