26f49ec355948281f78caae52805e35c139b9230
zeno9797 <126583332+zeno9797@users.noreply.github.com>
2023-04-08 11:04:01 +0200

    Add files via upload

---
+++ esercizio-11-array-methods/README.md
@@ -0,0 +1,3 @@
+# Array Methods - Exercise 53
+
+Create a function called `adultFilter` that takes in an array of people and returns the ones who are of age.
---
+++ esercizio-11-array-methods/exercise-1.js
@@ -0,0 +1,22 @@
+function adultFilter(persons) {
+  const filter = persons.filter(persons => persons.age >= 18)
+  return filter
+
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const adults = adultFilter(persons);
+console.log(persons);
+console.log(adults);
