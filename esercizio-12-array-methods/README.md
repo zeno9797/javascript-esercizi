bc9a93c17d6bf16a0208463584cc9ca28046c3b4
zeno9797 <126583332+zeno9797@users.noreply.github.com>
2023-04-08 11:04:35 +0200

    Add files via upload

---
+++ esercizio-12-array-methods/README.md
@@ -0,0 +1,9 @@
+# Array Methods - Exercise 54
+
+Create a function called `nicknameMap` that takes in an array of people and returns an array of nicknames. The nickname should be composed in this way: `<name>-<age>`.
+
+Example:
+
+```
+{ name: 'Paul', age: 21 } => Paul-21
+```
---
+++ esercizio-12-array-methods/exercise-2.js
@@ -0,0 +1,21 @@
+function nicknameMap(persons) {
+  const  map1 = persons.map(persons => persons.name + '-' + persons.age )
+  return map1
+  }
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 }
+];
+
+const nicknames = nicknameMap(persons);
+console.log(persons);
+console.log(nicknames);
