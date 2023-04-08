318a34aaffb8ea7bb0e25e8b60163076e99fccb2
zeno9797 <126583332+zeno9797@users.noreply.github.com>
2023-04-08 11:13:23 +0200

    Add files via upload

---
+++ esercizio-14-array-methods/README.md
@@ -0,0 +1,7 @@
+# Exercise 56
+
+Create a function called `uncompletedNotes` that returns only not completed todos.
+
+**Suggestion**
+
+Use the forEach and filter arrays methods.
---
+++ esercizio-14-array-methods/exercise-4.js
@@ -0,0 +1,57 @@
+function uncompletedNotes(notes) {
+  notes.forEach(element => {
+    console.log(element.todos.filter(element => element.done == false))
+  })
+}
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+uncompletedNotes(notes);
