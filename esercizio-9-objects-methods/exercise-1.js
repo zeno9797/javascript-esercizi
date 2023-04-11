d46681f27ad7110267ab0034d7e6bd0da01ee088
zeno9797 <126583332+zeno9797@users.noreply.github.com>
2023-04-11 19:53:31 +0200

    Add files via upload

---
+++ esercizio-9-objects-methods/exercise-1.js
@@ -0,0 +1,15 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+for (const key in person) {
+  console.log(key, person[key]);
+  
+}
+return person
+
+console.log(Object.keys(person[key]));
+
+// Print values of person using Object.keys
